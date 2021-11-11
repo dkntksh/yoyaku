dbebf936d60f29667056b6e47c4ee1353270ee5f
dnkntksh <mr.kaz71@gmail.com>
2021-11-11 22:46:50 +0900

    エラー処理の追加

--- lib/batch/service/create_initial_admin_user_service.rb
+++ lib/batch/service/create_initial_admin_user_service.rb
@@ -10,17 +10,25 @@ module Batch
 
       def execute
         logger.debug "#{self.class.name}\##{__method__} execute start ---"
-        # 部署 rootの作成
-        department = Department.new({name: 'root', department_code: '#root'})
-        department.save!
-        # 管理Userの作成
-        admin_user = AdminUser.new({department_id: department.id, name: 'root_admin'})
-        admin_user.save!
-        password = SecureRandom.hex(4)
-        admin_user.set_password(password)
-        logger.info "!!!!!----- create admin user! user_name:root_admin password:#{password} -----!!!!!"
-        # 作成データのログ出力
+        ApplicationRecord.transaction do
+          # 部署 rootの作成
+          department = Department.new({name: 'root', department_code: '#root'})
+          department.save!
+          # 管理Userの作成
+          admin_user = AdminUser.new({department_id: department.id, name: 'root_admin'})
+          admin_user.save!
+          password = SecureRandom.hex(4)
+          admin_user.set_password(password)
+          # 作成データのログ出力
+          logger.info "!!!!!----- create admin user! user_name:root_admin password:#{password} -----!!!!!"
+        end
         logger.debug "#{self.class.name}\##{__method__} execute end ---"
+      rescue ActiveRecord::RecordInvalid => are
+        logger.error are.message
+        logger.error are.backtrace.join("\n") 
+      rescue => e
+        logger.error e.message
+        logger.error e.backtrace.join("\n") 
       end
 
       private
