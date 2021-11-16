fb8f6e83374ca228487210712b68fa258dacdc3c
dnkntksh <mr.kaz71@gmail.com>
2021-11-16 22:19:49 +0900

    ログイン成功時、ホーム画面に遷移する

--- app/controllers/management_portal/login_controller.rb
+++ app/controllers/management_portal/login_controller.rb
@@ -4,16 +4,23 @@ class ManagementPortal::LoginController < ApplicationController
 
   def login
     name = login_params[:name]
-    admin_user = AdminUser.where(name: name)
-
+    admin_user = AdminUser.find_by(name: name)
     # パスワードの認証
     if admin_user.present? 
       if admin_user.authenticate(login_params[:password])
+        # ログイン処理
         session[:admin_user_id] = admin_user.id
         session[:admin_user_name] = admin_user.name
+        admin_user.login_error_count = 0
+        admin_user.save!
       else
-        admin_user.error
-        @error = 'ユーザが存在しないか、パスワードが誤っています。'
+        admin_user.login_error_count = admin_user.login_error_count.to_i + 1
+        if admin_user.login_error_count >= 5
+          admin_user.lock_datetime_to = Time.now + 10.minute
+          @error = 'ユーザがロックされています。10分後にアクセスしてください。'
+        else
+          @error = 'ユーザが存在しないか、パスワードが誤っています。'
+        end
         render :index
       end
     else
@@ -21,10 +28,8 @@ class ManagementPortal::LoginController < ApplicationController
       render :index
     end
 
-    # ログイン処理
-
     # home画面へ遷移
-    redirect_to home
+    redirect_to management_portal_home_path
   end
 
   def login_params
