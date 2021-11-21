5cc4888c0fc74e0d7e36268b999683878aeb9e43
dnkntksh <mr.kaz71@gmail.com>
2021-11-21 18:51:49 +0900

    エラーの解消

--- app/controllers/management_portal/departments_controller.rb
+++ app/controllers/management_portal/departments_controller.rb
@@ -2,6 +2,8 @@ class ManagementPortal::DepartmentsController < ApplicationController
   before_action :is_management_portal_login?
 
   def index
+    # TODO:初期表示の取得は要修正
+    @departments = Department.all
   end
 
   def create
@@ -11,6 +13,9 @@ class ManagementPortal::DepartmentsController < ApplicationController
   end
 
   def detail
+  end
+
+  def delete
   end
   
 end
