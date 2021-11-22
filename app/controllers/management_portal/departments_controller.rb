ffbd221dfdcf210208b1f63ef1a0c25b214cb96b
dnkntksh <mr.kaz71@gmail.com>
2021-11-22 19:23:03 +0900

    一時コミット

--- app/controllers/management_portal/departments_controller.rb
+++ app/controllers/management_portal/departments_controller.rb
@@ -6,16 +6,26 @@ class ManagementPortal::DepartmentsController < ApplicationController
     @departments = Department.all
   end
 
+  def new
+    @departments = Department.new
+  end
+
   def create
   end
 
   def edit
+    @departments = Department.find(params[:id])
   end
 
   def detail
   end
 
   def delete
+  end
+
+  private
+  def department_params
+    params.require(:department).permit(:name, :department_code)
   end
   
 end
