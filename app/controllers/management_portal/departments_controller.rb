445a836b70be34666880a23f5438c30081b670fe
dnkntksh <mr.kaz71@gmail.com>
2021-11-26 22:23:38 +0900

    登録エラーの修正

--- app/controllers/management_portal/departments_controller.rb
+++ app/controllers/management_portal/departments_controller.rb
@@ -13,15 +13,18 @@ class ManagementPortal::DepartmentsController < ApplicationController
   def create
     begin
      @department = Department.new(department_params)
-     if @department.valid?
-      Rails.logger.error @department.errors
+     if @department.invalid?
+      Rails.logger.error 'valid error'
+      Rails.logger.error @department.errors.full_messages
       render management_portal_departments_new_path and return
     end
      @department.save!
     rescue ActiveRecord::RecordInvalid
+      Rails.logger.error 'ActiveRecord::RecordInvalid error'
       Rails.logger.error @department.errors
       render management_portal_departments_new_path and return
     rescue => e
+      Rails.logger.error 'other error'
       Rails.logger.error e.message
       Rails.logger.error e.backtrace
       render management_portal_departments_new_path and return
