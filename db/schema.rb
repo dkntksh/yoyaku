90a0de901e7385c9427eaa65509fb432a63d48d6
dnkntksh <mr.kaz71@gmail.com>
2021-12-11 16:44:12 +0900

    会議室モデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_094458) do
+ActiveRecord::Schema.define(version: 2021_12_11_074252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_12_05_094458) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["department_code"], name: "index_departments_on_department_code", unique: true
+  end
+
+  create_table "meeting_rooms", force: :cascade do |t|
+    t.string "name"
+    t.string "place"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
